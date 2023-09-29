1110a6de8288502c0246a7402c5effc1993c415e
Reid Roberts <reidrobertscs@gmail.com>
2023-09-28 20:48:07 -0500

    Create predict_restructuring.js

---
+++ predict_restructuring.js
@@ -0,0 +1,72 @@
+// Problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// randomCar ->  'Tesla'
+// otherRandomCar -> 'Mercedes'
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(name);
+console.log(otherName);
+
+// name -> will throw a ReferenceError since "name" isn't defined
+// otherName -> 'Elon'
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Problem 3
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+// password -> '12345'
+// hashedPassword -> undefined since 
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Problem 4
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+// first == second -> false
+// first == third -> true
+
+//////////////////////////////////////////////////////////////////////////////////////
+// Problem 5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+// key -> 'value'
+// secondKey -> [1, 5, 1, 8, 3, 3]
+// secondKey[0] -> 1
+// willThisWork -> 5
+

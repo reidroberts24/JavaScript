a7bafa1c4f18e84bdccb66f556f9a7d9a72ee4e5
Reid Roberts <reidrobertscs@gmail.com>
2023-10-17 18:52:26 -0500

    commit

---
+++ practice/Consuming APIs/pokemon.js
@@ -0,0 +1,10 @@
+fetch("http://www.example.com")
+    .then(response =>{
+        //do something
+    }).catch(err => {
+        console.log(err);
+    })
+
+
+let response = await fetch("http://www.example.com");
+

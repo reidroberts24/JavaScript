045bc862a8b4329181232f6fc83391d0608c49e9
Reid Roberts <reidrobertscs@gmail.com>
2023-10-18 20:14:37 -0500

    added components

--- practice/react-router-walkthrough/src/App.jsx
+++ practice/react-router-walkthrough/src/App.jsx
@@ -2,6 +2,26 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+
+const Home = (props) => {
+  return(
+    <div>
+      <h1 style={{ color: "red" }}>
+        Home Component
+      </h1>
+    </div>
+  )
+}
+
+const About = (props) => {
+  return(
+    <div>
+      <h1 style={{ color: "blue" }}>
+        About Component
+      </h1>
+    </div>
+  )
+}
 function App() {
   const [count, setCount] = useState(0)
 
@@ -9,7 +29,7 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Routes>
-        <Route path="/"/>
+        <Route path="/" element="" />
       </Routes>
     </div>  
     </BrowserRouter>

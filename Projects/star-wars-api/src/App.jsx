28fff0c2a66f7152e69f4226f41675b7c09cd732
Reid Roberts <reidrobertscs@gmail.com>
2023-10-18 23:21:17 -0500

    commit

---
+++ Projects/star-wars-api/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  )
+}
+
+export default App

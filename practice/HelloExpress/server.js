75418e3ef2fdfb9de9ad1eb6a88536f1c6c65462
Reid Roberts <reidrobertscs@gmail.com>
2023-10-23 19:30:05 -0500

    created new User object

--- practice/HelloExpress/server.js
+++ practice/HelloExpress/server.js
@@ -1,12 +1,29 @@
 const express = require("express");
+const faker = require('faker');
+
 const app = express();
 const port = 8000;
 
-// req is short for request
-// res is short for response
 app.get("/api", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
-// this needs to be below the other code blocks
 app.listen( port, () => console.log(`Listening on port: ${port}`) ); 
+
+
+
+const createUser = () => ({
+  userPassword: faker.internet.password(),
+  userEmail: faker.internet.email(),
+  userPhoneNumber: faker.phone.number(),
+  userLastName: faker.person.lastName(),
+  userFirstName: faker.person.firstName(),
+  userID: faker.string.uuid(),
+
+});
+
+
+app.get("/api/users/new", (req, res) => {
+  newUser = createUser();
+  res.json(newUser)
+})

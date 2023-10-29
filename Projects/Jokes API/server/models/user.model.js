d1bfa82dad872005e3a9a761588622c2acedaefa
Reid Roberts <reidroberts24@gmail.com>
2023-10-29 18:51:25 -0500

    created db schema

---
+++ Projects/Jokes API/server/models/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type:String
+    },
+    age: {
+        type: Number
+    }
+});
+
+const User = mongoose.model('User', UserSchema)
+module.expost = User;

582f514ecc2027efee84a4ef31d56dd21deb9e1d
Reid Roberts <reidroberts24@gmail.com>
2023-10-30 11:45:39 -0500

    restructured folders

--- Projects/Jokes API/server/config/mongoose.config.js
+++ Projects/Jokes API/server/config/mongoose.config.js
@@ -1,8 +1,1 @@
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://127.0.0.1:27017/my_first_db', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-    .then( () => console.log('Established a connection to the database'))
-    .catch( err => console.log('Something went wrong connecting to the database: ', err));
+const mongoose = require('mongoose')

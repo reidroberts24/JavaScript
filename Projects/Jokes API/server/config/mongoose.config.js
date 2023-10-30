6c2ed645a991195f5d5fc482aba4a854043b2cab
Reid Roberts <reidroberts24@gmail.com>
2023-10-30 11:48:20 -0500

    created jokes API project

--- Projects/Jokes API/server/config/mongoose.config.js
+++ Projects/Jokes API/server/config/mongoose.config.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/jokes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then( () => console.log('Established a connection to the database'))
+    .catch( err => console.log('Something went wrong connecting to the database: ', err));

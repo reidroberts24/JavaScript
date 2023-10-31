22104549859ee1861f8625fa0b3a95330e8830f8
Reid Roberts <reidroberts24@gmail.com>
2023-10-31 18:22:46 -0500

    craeted routes

--- practice/fullStackMERN/server/routes/person.routes.js
+++ practice/fullStackMERN/server/routes/person.routes.js
@@ -1,5 +1,6 @@
 const PersonController = require('../controllers/person.controller');  //Import the code from Code Block 1
 module.exports = (app) => {
     app.get('/api', PersonController.index);
+    app.post('/api/people', PersonController.createPerson)
 }
 

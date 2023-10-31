ae6d65d5ca74d744a796d2aab975af7d18bb641c
Reid Roberts <reidroberts24@gmail.com>
2023-10-31 18:38:34 -0500

    fixed controller methods

--- practice/fullStackMERN/server/controllers/person.controller.js
+++ practice/fullStackMERN/server/controllers/person.controller.js
@@ -6,8 +6,11 @@ module.exports.index = (request, response) => {  //We are exporting a key:val pa
 
 const Person = require('../models/person.model')
 
+
+// req.body is for all the JSON data
+// req.params is for specific route parameters like /api/:id
 module.exports.createPerson = (req, res) => {
-    personInfo = req.params.body;
+    personInfo = req.body;
     Person.create(personInfo) //returns a Promise 
         .then(person => res.json(person)) //when the Promise is resolved. it takes the created person as the argument and sends back as a JSON response
         .catch(err => res.json(err))
--- practice/fullStackMERN/server/server.js
+++ practice/fullStackMERN/server/server.js
@@ -1,9 +1,13 @@
-
 const cors = require('cors')
 const express = require('express');
 const app = express();
-app.use(cors())
 const port = 8000;
-    
+
+app.use(cors())
+app.use(express.json()) //allows json objects to be posted
+app.use(express.urlencoded( { extended: true } )) //allows json objects with strings and arrays
+require('./config/mongoose.config')    
 require('./routes/person.routes')(app)
 app.listen(port, () => console.log(`Listening on port: ${port}`) );
+
+//make sure back end logic works and stores data before working on the front-end

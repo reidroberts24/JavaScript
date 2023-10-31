b6a558924eff9ca1dfa32f7137e592da14f1a291
Reid Roberts <reidroberts24@gmail.com>
2023-10-31 18:21:53 -0500

    created controller

--- practice/fullStackMERN/server/controllers/person.controller.js
+++ practice/fullStackMERN/server/controllers/person.controller.js
@@ -6,6 +6,9 @@ module.exports.index = (request, response) => {  //We are exporting a key:val pa
 
 const Person = require('../models/person.model')
 
-module.exports.index = (request, response) => {
-    
+module.exports.createPerson = (req, res) => {
+    personInfo = req.params.body;
+    Person.create(personInfo) //returns a Promise 
+        .then(person => res.json(person)) //when the Promise is resolved. it takes the created person as the argument and sends back as a JSON response
+        .catch(err => res.json(err))
 }

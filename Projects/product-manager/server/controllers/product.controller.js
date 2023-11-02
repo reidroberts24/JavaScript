1fc9a6b91c80f19d1184ec95af1dc7697226ff3f
Reid Roberts <reidroberts24@gmail.com>
2023-11-01 19:26:15 -0500

    added getter route and method

--- Projects/product-manager/server/controllers/product.controller.js
+++ Projects/product-manager/server/controllers/product.controller.js
@@ -19,3 +19,10 @@ module.exports.getAllProducts = (req, res) => {
             res.json(err)
         })
 }
+
+module.exports.getOneProduct = (req, res) => {
+    Product.findOne({ _id: req.params.id} ) // "_id" is for mongoose. But in req.params.id we can name it whatever we want after params....it just needs to match our route
+        .then( product => res.json(product) )
+        .catch( err => res.json(err) )
+}
+
--- Projects/product-manager/server/routes/product.routes.js
+++ Projects/product-manager/server/routes/product.routes.js
@@ -2,5 +2,6 @@ const ProductController = require('../controllers/product.controller')
 
 module.exports = (app) => {
     app.get('/api/products', ProductController.getAllProducts)
+    app.get('/api/products/:id', ProductController.getOneProduct) //the ":id" needs to match the parameter name in req.params.id <- id matched
     app.post('/api/products', ProductController.createProduct)
 }
